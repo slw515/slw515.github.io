b0f86cd7056f76125ead6679d490aed10bbc373b
Steven Wyks <stevenwyks@yahoo.com>
2019-11-17 22:56:09 +0400

    swap test

--- space-dash/server.js
+++ space-dash/server.js
@@ -1,3 +1,74 @@
+// // console.log("sever");
+// var fs = require("fs");
+// var data = fs.readFileSync("words.json");
+// var words = JSON.parse(data);
+// console.log(words);
+// var express = require("express");
+
+// var app = express();
+
+// var server = app.listen(3000, listening);
+// function listening() {
+//   console.log("listening...");
+// }
+
+// app.use(express.static("website"));
+
+// app.get("/add/:name/:score?", addScore);
+
+// function addScore(request, response) {
+//   var data = request.params;
+//   var name = data.name;
+//   var score = Number(data.score);
+
+//   if (!score) {
+//     var reply = {
+//       msg: "Score required"
+//     };
+//     response.send(reply);
+//   } else {
+//     words[name] = score;
+//     var data = JSON.stringify(words, null, 2);
+//     fs.writeFile("words.json", data, finished);
+//     function finished(err) {
+//       console.log("data is in");
+//     }
+//     var reply = {
+//       msg: "thank you!"
+//     };
+//     response.send(reply);
+//   }
+// }
+
+// app.get("/all", sendAll);
+// function sendAll(request, response) {
+//   response.send(words);
+// }
+// app.get("/search/:name/", searchScore);
+
+// function searchScore(request, response) {
+//   var name = request.params.name;
+//   var reply;
+//   if (words[name]) {
+//     reply = {
+//       status: "found",
+//       name: name,
+//       score: words[name]
+//     };
+//   } else {
+//     reply = {
+//       status: "not found",
+//       name: name
+//     };
+//   }
+//   response.send(reply);
+// }
+// console.log("yes");
+// $.post("words.json", { json: JSON.stringify(json) }, function(data) {
+//   var allQuestions = data;
+//   console.log(allQuestions);
+// });
+
 const express = require("express");
 const Datastore = require("nedb");
 const app = express();
